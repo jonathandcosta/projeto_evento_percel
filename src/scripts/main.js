52414c20187650ca272e012522380723fde018ca
Jonathan Costa <jonathanduartecosta@gmail.com>
2023-02-20 11:51:50 -0300

    adicionando arquivo JS com o contador de D H M S

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,42 @@
 AOS.init();
+
+//dia do evento
+const dataDoEvento = new Date("Feb 28, 2023 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+//contagem das horas para o evento
+const contaAsHoras = setInterval(function () {
+  const agora = new Date();
+  const timeStampAtual = agora.getTime();
+
+  const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+  const diaEmMs = 1000 * 60 * 60 * 24;
+  const horaEmMs = 1000 * 60 * 60;
+  const minutoEmMs = 1000 * 60;
+
+  const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+  const horasAteOEvento = Math.floor(
+    (distanciaAteOEvento % diaEmMs) / horaEmMs
+  );
+  const minutosAteOEvento = Math.floor(
+    (distanciaAteOEvento % horaEmMs) / minutoEmMs
+  );
+  const segundosAteOEvento = Math.floor(
+    (distanciaAteOEvento % minutoEmMs) / 1000
+  );
+
+  console.log(diasAteOEvento);
+  console.log(horasAteOEvento);
+  console.log(minutosAteOEvento);
+  console.log(segundosAteOEvento);
+
+  document.getElementById(
+    "contador"
+  ).innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+  if (distanciaAteOEvento < 0) {
+    clearInterval(contaAsHoras);
+    document.getElementById("contador").innerHTML = "Evento expirado";
+  }
+}, 1000);
